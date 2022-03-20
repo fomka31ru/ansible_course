3b355b839c251a61657daac33ebd3e4c29a61ccc
Yury <Yury.Lukyanov@gmail.com>
2022-03-20 19:00:10 +0300

    practice 04 fix

--- practice_04/task_02/library/healthcheck_sh.sh
+++ practice_04/task_02/library/healthcheck_sh.sh
@@ -5,7 +5,7 @@ addr=$(cat "$1" | grep -Po '(?<="addr": ")(.*?)(?=")')
 tls=$(cat "$1" | grep -Po '(?<="tls": )(.*?)(?=,)')
 
 # return=$(curl -sIL -k "${addr}" | grep ^HTTP | tail -1)
-return=$(wget --server-response --spider --quiet "${addr}" 2>&1 | grep HTTP | tail -1 | sed -r 's/^ *| *$//g')
+return=$(wget --server-response --no-check-certificate --spider --quiet "${addr}" 2>&1 | grep HTTP | tail -1 | sed -r 's/^ *| *$//g')
 return_code=$(echo "${return}" | cut -d' ' -f2 )
 return_status=$(echo "${return}" | cut -d' ' -f3- | tr -d '\r')
 
